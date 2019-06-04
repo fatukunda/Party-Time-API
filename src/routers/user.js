ec6cccf3c7d219f478f89e231e8f943be95b2272
Frank Atukunda <fatukunda@gmail.com>
2019-06-04 18:18:51 +0300

    Feature(Delete user profile)
     - Create `DELETE /users/me` router
     - Create `POST /users/me/logout` router
     - Create `POST /users/me/logoutall`
    
     [Delivers: #166437908]

--- src/routers/user.js
+++ src/routers/user.js
@@ -52,4 +52,38 @@ router.patch('/users/me', auth, async(req, res) => {
     }
 })
 
+router.delete('/users/me', auth, async (req, res) => {
+    // Delete a user from the application
+    try {
+        await req.user.remove()
+        res.send(req.user)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+router.post('/users/me/logout', auth, async (req, res) => {
+    // Log user out of the application
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token != req.token
+        })
+        await req.user.save()
+        res.send()
+    } catch (error) {
+        res.status(500).send()
+    }
+})
+
+router.post('/users/me/logoutall', auth, async(req, res) => {
+    // Log user out of all devices
+    try {
+        req.user.tokens.splice(0, req.user.tokens.length)
+        await req.user.save()
+        res.send()
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
 module.exports = router
